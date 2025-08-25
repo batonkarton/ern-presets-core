046f7b68a612a885274eb5a185c056e839a343b2
unknown <rxtmark@gmail.com>
2025-08-25 11:35:30 +0300

    LPC-4: Added test solution folder and first test

---
+++ LoopstationCompanionApi.Tests/PresetTests.cs
@@ -0,0 +1,14 @@
+using LoopstationCompanionApi.Models;
+
+namespace LoopstationCompanionApi.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Preset_Should_Have_Default_DeviceModel()
+        {
+            var preset = new Preset { Name = "Test" };
+            Assert.Equal(DeviceModel.RC505mkII, preset.DeviceModel);
+        }
+    }
+}
